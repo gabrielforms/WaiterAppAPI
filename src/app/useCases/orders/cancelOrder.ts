63c2e9e27e935a13e3f1896d3f2a3e5cbafbb2d6
gabrielforms <gabrielformiga3@gmail.com>
2023-02-27 01:34:47 -0300

    Adicionando api do waiter App para o bitbucket

---
+++ src/app/useCases/orders/cancelOrder.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import { Order } from '../../models/Order';
+
+export async function cancelOrder(req: Request, res: Response) {
+  try{
+    const { orderId } = req.params;
+
+;    await Orderm.findByIdAndDelete(orderId);
+    res.sendStatus(204);
+  } catch (error){
+    console.error(error);
+    res.sendStatus(500);
+  }
+}
